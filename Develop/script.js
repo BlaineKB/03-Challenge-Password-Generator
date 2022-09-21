e10d015dae160937b4b24f115dece757b230b348
BKB <blainekb94@gmail.com>
2022-09-21 01:42:35 -0500

    added initial variables and first randomizer functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,33 @@
-// Assignment code here
+// Select the generate password button
+var generateBtn = document.querySelector("#generate")
+
+// Keys with value pairs of functions that generate random characters
+var randomCharacters = {
+  charSymbol: genRandomSymbol,
+	charNum: genRandomNumber,
+  charUpper: genRandomUpper,
+  charLower: genRandomLower, 
+};
+
+function genRandomSymbol(){
+  var symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols [Math.floor(Math.random() * symbols.length)]
+}
+
+function genRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 45)
+}
+
+function genRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 77)
+}
+
+// Functions for generating random characters
+function genRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 88)
+}
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
