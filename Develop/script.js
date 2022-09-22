9eb74553600c99a5a3558a68a51534dd2214a73e
BKB <blainekb94@gmail.com>
2022-09-22 17:28:53 -0500

    last updates to code

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,6 @@ var generateBtn = document.querySelector("#generate")
 
 // Function for generating password with random characters
 function generatePassword() {
-  console.log("Clicked")
   var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var charNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
