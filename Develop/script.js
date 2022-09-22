db4a89783c8e763a665a56b059beb71510a7995a
BKB <blainekb94@gmail.com>
2022-09-22 17:26:06 -0500

    fixed writePassword function to display generated password and added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Select the generate password button
 var generateBtn = document.querySelector("#generate")
 
-// Function for generating password
+// Function for generating password with random characters
 function generatePassword() {
   console.log("Clicked")
   var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -9,12 +9,12 @@ function generatePassword() {
   var charNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var charSymbol = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "="]
 
-  // Prompt user for password criteria, 
+  // Prompt user for all of the password criteria
 
-  var password1 = [];
+  var newPassword = [];
   var input = [];
 
-  // password length between 8-128, 
+  // Prompts user with a password length question that must be between 8 and 128 characters
 
   var length = prompt ("Choose a password length between 8 and 128 characters.");
 
@@ -24,64 +24,41 @@ function generatePassword() {
     return
   }
 
-  // lowercase uppercase numbers special characters
+  // Prompt questions that user must select in order to generate password
 
   var lowercaseQuestion = confirm ("Would you like your password to contain lowercase letters?");
   var uppercaseQuestion = confirm ("Would you like your password to contain uppercase letters?");
   var numberQuestion = confirm ("Would you like your password to contain numbers?");
-  var specialQuestion = confirm ("Would you like your password to contain special characters?");
+  var symbolQuestion = confirm ("Would you like your password to contain special characters?");
 
-  // Validate the layout
+  // Joins user prompt selections into final password
 
-  if (numberQuestion){password1 = password1.concat(number);
+  if (numberQuestion){newPassword = newPassword.concat(charNumber);
   }
   
-  if (uppercaseQuestion){password1 = password1.concat(uppercase);
+  if (uppercaseQuestion){newPassword = newPassword.concat(charUpper);
   }
   
-  if (lowercaseQuestion){password1 = password1.concat(lowercase);
+  if (lowercaseQuestion){newPassword = newPassword.concat(charLower);
   }
   
-  if (specialQuestion){password1 = password1.concat(special);
+  if (symbolQuestion){newPassword = newPassword.concat(charSymbol);
   }
   
   
 
-  // Generate password
+  // Randomly generates password including the user prompt selections
   
   for (var i = 0; i < length; i++) {
       
-    input.push (password1[Math.floor(Math.random() * password1.length)]); 
-    }
-  
-    return input.join("") ;
+    input.push (newPassword[Math.floor(Math.random() * newPassword.length)]); 
   }
   
-
-
-  // Display password
-
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  return input.join("");
+}
   
-    passwordText.value = password;
 
-}
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
